e4e52ca9cb73f49b96c706179b8ca4abc9fefa1f
cybercoder-naj <cybercoder.nishant@gmail.com>
2021-08-28 14:25:14 +0530

    Finished the website

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,42 @@
-const changeNavBehaviour = () => {
-    const nav = document.querySelector('nav');
-
-    if (window.innerWidth < 768) {
-        nav.classList.add('bg-dark');
-        return;
-    }
-
-    nav.classList.remove('bg-dark');
-    if (window.scrollY > 60)
-        nav.style.backgroundColor = 'black';
-    else
-        nav.style.backgroundColor = 'transparent';
-};
-
 $(function () {
     $(document).click(function () {
         $('.navbar-collapse').collapse('hide');
     });
 
-    changeNavBehaviour();
-});
+    const changeNavBehaviour = function() {
+        const nav = $('nav');
+        
+        if ($(window).width() < 768) {
+            nav.addClass('bg-dark');
+            return;
+        }
+    
+        nav.removeClass('bg-dark');
+        nav.css('background-color', ($(window).scrollTop() > 60) ? 'black' : 'transparent');
+    };
 
-const buttonToTop = () => {
-    const button = document.getElementById('top-button')
-    button.style.opacity = (window.scrollY > 60) ? '1' : '0';
-    button.style.zIndex = (window.scrollY > 60) ? '1030' : '-1030';
-}
-
-$(window).scroll(function() {
     changeNavBehaviour();
-    buttonToTop();
+
+    $(window).scroll(function() {
+        changeNavBehaviour();
+        
+        $('#top-button').css({
+            'opacity': ($(window).scrollTop() > 60) ? '1' : '0',
+            'z-index': ($(window).scrollTop() > 60) ? '1030' : '-1030'
+        });
+    });
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+    
+        const name = $('form #name').val();
+        const email = $('form #email').val();
+        const message = $('form #message').val();
+
+        const subject = `A message from ${name}`;
+        const body = `Name: ${name}%0D%0AEmail: ${email}%0D%0A %0D%0A${message}`;
+
+        window.location.replace(`mailto:cybercoder.nishant@gmail.com?subject=${subject}&body=${body}`)
+        e.currentTarget.submit();
+    });
 });
