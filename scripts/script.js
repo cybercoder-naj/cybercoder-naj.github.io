d31a9fe8f6e3bcf4183b61a6046637b30ea1e22c
cybercoder-naj <cybercoder.nishant@gmail.com>
2022-05-25 23:57:39 +0530

    Added Medium Articles section

--- scripts/script.js
+++ scripts/script.js
@@ -3,32 +3,32 @@ $(function () {
         $('.navbar-collapse').collapse('hide');
     });
 
-    const changeNavBehaviour = function() {
+    const changeNavBehaviour = function () {
         const nav = $('nav');
-        
+
         if ($(window).width() < 768) {
             nav.addClass('bg-dark');
             return;
         }
-    
+
         nav.removeClass('bg-dark');
         nav.css('background-color', ($(window).scrollTop() > 60) ? 'black' : 'transparent');
     };
 
     changeNavBehaviour();
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         changeNavBehaviour();
-        
+
         $('#top-button').css({
             'opacity': ($(window).scrollTop() > 60) ? '1' : '0',
             'z-index': ($(window).scrollTop() > 60) ? '1030' : '-1030'
         });
     });
 
-    $('form').submit(function(e) {
+    $('form').submit(function (e) {
         e.preventDefault();
-    
+
         const name = $('form #name').val();
         const email = $('form #email').val();
         const message = $('form #message').val();
@@ -40,7 +40,32 @@ $(function () {
         e.currentTarget.submit();
     });
 
-    $('.project-card button').click(function() {
+    $('.project-card button').click(function () {
         window.location.href = $(this).data('url');
     })
+
+    async function loadMediumArticles() {
+        const response = await fetch('https://api.rss2json.com/v1/api.json?rss_url=https://www.medium.com/feed/@cybercoder.naj')
+        const data = await response.json()
+
+        $(".article-card").each((i, article) => {
+            $('<img>')
+                .attr('src', data.items[i].thumbnail)
+                .attr('alt', data.items[i].title)
+                .appendTo(article)
+
+            $('<h3>')
+                .addClass('font-weight-bold p-3')
+                .html(data.items[i].title)
+                .appendTo(article)
+
+            $('<a>')
+                .addClass('m-3 py-2 px-3')
+                .attr('data-url', data.items[i].link)
+                .html('View Article')
+                .appendTo(article)
+        })
+    }
+
+    loadMediumArticles()
 });
