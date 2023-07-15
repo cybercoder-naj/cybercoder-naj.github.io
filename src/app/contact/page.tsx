a73452bbee4c1b2f71c1a1435c87d904acca43f1
Nishant <cybercoder.nishant@gmail.com>
2023-07-16 00:19:28 +0100

    added menu expanded code to contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -4,10 +4,11 @@ import { useEffect } from "react"
 import { useGlobalContext } from "../Context/store"
 
 export default function Contact() {
-    const { page, setPage } = useGlobalContext()
+    const { page, setPage, setMenuExpanded } = useGlobalContext()
 
     useEffect(() => {
         setPage('contact')
+        setMenuExpanded(false)
     }, [])
 
     return <h1>Contact</h1>
